92d8ba98b0d7df084be0cdc7be4e3415f5c3bfc7
Andrii Antonov <pro100svitlo@gmail.com>
2023-02-28 21:29:40 +0100

    feat: add `home_list_item_data` to the homePageState

--- application/lib/presentation/view/screen/main/page/home/manager/home_page_state.dart
+++ application/lib/presentation/view/screen/main/page/home/manager/home_page_state.dart
@@ -3,4 +3,9 @@ part of 'home_page_manager.dart';
 @freezed
 abstract class HomePageState with _$HomePageState {
   const factory HomePageState.initial() = _Initial;
+
+  @Assert('dataItems.isNotEmpty')
+  const factory HomePageState.ready({
+    required List<HomeListItemData> dataItems,
+  }) = HomePageStateReady;
 }
