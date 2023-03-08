6a2d84b100bbac5c3f08028beb9c5068a8447f12
Andrii Antonov <pro100svitlo@gmail.com>
2023-03-08 19:59:17 +0100

    feat: create labeledChild and labeledChildren widgets

---
+++ application/lib/presentation/widget/labeled_child.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/presentation/resource/r.dart';
+import 'package:mobile_app/presentation/view/screen/settings/widget/settings_section_label.dart';
+
+class LabeledChild extends StatelessWidget {
+  final String label;
+  final TextStyle? labelStyle;
+  final Widget child;
+  final double horizontalChildPadding;
+
+  LabeledChild({
+    required this.label,
+    required this.child,
+    double? horizontalChildPadding,
+    this.labelStyle,
+    super.key,
+  })  : assert(label.isNotEmpty),
+        horizontalChildPadding =
+            horizontalChildPadding ?? R.dimen.screenPaddingSide;
+
+  @override
+  Widget build(BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SettingsSectionLabel(
+            label: label,
+            labelStyle: labelStyle,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: horizontalChildPadding,
+            ),
+            child: child,
+          )
+        ],
+      );
+}
+
+class LabeledChildren extends LabeledChild {
+  LabeledChildren({
+    required super.label,
+    required List<Widget> children,
+    super.labelStyle,
+    super.horizontalChildPadding,
+    super.key,
+  })  : assert(children.isNotEmpty),
+        super(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: children,
+          ),
+        );
+}
