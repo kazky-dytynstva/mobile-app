33b0474c34e3ae098e8c7a625f0cc65db533f7e0
Andrii Antonov <pro100svitlo@gmail.com>
2023-04-08 09:05:02 +0200

    feat: save state when click app share

--- application/lib/data/use_case/share/share_app_use_case.dart
+++ application/lib/data/use_case/share/share_app_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:mobile_app/domain/data_source/remote_configs.dart';
+import 'package:mobile_app/domain/data_source/storage_local/app_state_storage.dart';
 import 'package:mobile_app/domain/extensions/string.dart';
 import 'package:mobile_app/domain/helper/share_helper.dart';
 import 'package:mobile_app/domain/use_case/usecase.dart';
@@ -10,14 +11,18 @@ import 'package:mobile_app/presentation/resource/r.dart';
 class ShareAppUseCase extends UseCase<Dry, ShareAppOutput> {
   final ShareHelper _shareHelper;
   final RemoteConfigs _remoteConfigs;
+  final AppStateStorage _stateStorage;
 
   ShareAppUseCase(
     this._shareHelper,
     this._remoteConfigs,
+    this._stateStorage,
   );
 
   @override
   Stream<ShareAppOutput> transaction(Dry input) async* {
+    await _stateStorage.setShareAppClicked();
+
     final appLink = _remoteConfigs.shareAppLink().get();
     final text = R.strings.general.shareApp.format(appLink);
 
