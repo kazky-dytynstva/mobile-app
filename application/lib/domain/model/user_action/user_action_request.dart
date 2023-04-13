01a7026b8f4068a87a988f9e4e52b8532d90344b
Andrii Antonov <pro100svitlo@gmail.com>
2023-04-13 19:31:44 +0200

    feat: add appUpdate user_action_request.dart

--- application/lib/domain/model/user_action/user_action_request.dart
+++ application/lib/domain/model/user_action/user_action_request.dart
@@ -1,10 +1,15 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mobile_app/domain/model/forced_update_info/forced_update_info.dart';
 import 'package:mobile_app/domain/model/menu_dynamic_item/menu_dynamic_item_data.dart';
 
 part 'user_action_request.freezed.dart';
 
 @Freezed()
 abstract class UserActionRequest with _$UserActionRequest {
+  const factory UserActionRequest.appUpdate({
+    required ForcedUpdateInfo updateInfo,
+  }) = _AppUpdate;
+
   const factory UserActionRequest.rate() = _Rate;
 
   const factory UserActionRequest.share() = _Share;
